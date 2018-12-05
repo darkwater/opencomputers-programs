93214ecb7bdfa1d2c5b262f89a6b95a7f6e7cdc9
Sam Lakerveld <darkwater124@gmail.com>
2018-12-05 23:12:32 +0100

    Update me-server

--- me-server/me-server.lua
+++ me-server/me-server.lua
@@ -4,10 +4,11 @@ while true do
     for n = 5, 7 do
         local item = component.me_controller.getItemsInNetwork({
             label = db.get(n).label
-        })
+        })[1]
 
-        if size > 0 then
+        if item and item.size > 0 then
             component.me_exportbus.setExportConfiguration(sides.top, 1, db.address, n)
+            component.me_exportbus.exportIntoSlot(sides.top, 1)
             break
         end
     end
