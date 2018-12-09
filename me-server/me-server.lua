33bfaff580106587bd58d0cfb8e88727bf78cde8
Sam Lakerveld <darkwater124@gmail.com>
2018-12-09 23:13:42 +0100

    Condense cobblestone

--- me-server/me-server.lua
+++ me-server/me-server.lua
@@ -1,19 +1,31 @@
+local component = require("component")
+local sides     = require("sides")
+
 while true do
-    local db = component.database
+    local ctrl = component.me_controller
+    local db   = component.database
+
+    local eb_inscriber = component.proxy(component.get("670a7"))
+    local eb_condenser = component.proxy(component.get("f0bc1"))
 
     for n = 5, 7 do
-        local item = component.me_controller.getItemsInNetwork({
+        local item = ctrl.getItemsInNetwork({
             label = db.get(n).label
         })[1]
 
         if item and item.size > 0 then
-            component.me_exportbus.setExportConfiguration(sides.top, 1, db.address, n)
-            component.me_exportbus.exportIntoSlot(sides.top, 1)
+            eb_inscriber.setExportConfiguration(sides.top, 1, db.address, n)
+            eb_inscriber.exportIntoSlot(sides.top, 1)
             break
         end
     end
 
-    component.me_exportbus.setExportConfiguration(sides.top, 1)
+    local cobble = ctrl.getItemsInNetwork({ label = "Cobblestone" })[1].size
+    if cobble > 1000000 then
+        eb_condenser.setExportConfiguration(sides.west, 1, db.address, 10)
+    else
+        eb_condenser.setExportConfiguration(sides.west, 1)
+    end
 
     os.sleep(1)
 end
